70e81536767f48466a1894b708b767f607f5fdbf
Mark Lewis <mark@fortressofgeekdom.org>
2005-04-30 20:22:14 +0000

    Merging in monsters from fog_test, since they are a little more advanced.

---
+++ src/games/hacknslash/monster_pack1.py
@@ -0,0 +1,29 @@
+import combat
+from combat import Monster
+
+def register_monsters():
+    combat.gallery.add_monster( range(1,2), Snotling ) 
+    combat.gallery.add_monster( range(1,3), LargeSlug ) 
+
+class Snotling(Monster):
+    def __init__(self):
+        self.name = "Snotling"
+        self.max_hp = 6
+        self.attack_damage = '1d4'
+        self.thaco = 20
+        self.ac = 10
+        self.agility = 14
+        self.exp_value = 2
+        self.gold = '1d6'
+
+class LargeSlug(Monster):
+    def __init__(self):
+        self.name = "Giant Slug"
+        self.max_hp = 10
+        self.attack_damage = '2d3'
+        self.thaco = 18
+        self.ac = 12
+        self.agility = 10
+        self.exp_value = 3
+        self.gold = '2d4'
+
